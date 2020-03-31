944ebf187f6edf0d55d10b668693e24666deb2ae
Rafael Rauber <rrafaelfr2@gmail.com>
2020-03-31 01:26:11 -0400

    Commit Inicial

---
+++ sql/monuments_startup.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS `monuments_startup`;
+CREATE TABLE `monuments_startup` (
+  `obj_id` decimal(10,0) DEFAULT NULL,
+  `pvp` decimal(10,0) DEFAULT NULL,
+  `pk` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
