7370f5cddd059a55bd47e51e22505a198a2753d4
Rafael Rauber <rrafaelfr2@gmail.com>
2020-04-05 11:44:58 -0400

    Ajustes de teleport

--- build/dist/sql/monuments_startup.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `monuments_startup`;
-CREATE TABLE `monuments_startup` (
-  `obj_id` decimal(10,0) DEFAULT NULL,
-  `pvp` decimal(10,0) DEFAULT NULL,
-  `pk` decimal(10,0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
